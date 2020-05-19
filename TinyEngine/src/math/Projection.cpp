382fe03d9bcf3519cfb14e3b34103b049d36d1ec
Norbert Nopper <nopper@ux3d.io>
2020-05-19 18:18:53 +0200

    Added viewport projection.

--- TinyEngine/src/math/Projection.cpp
+++ TinyEngine/src/math/Projection.cpp
@@ -72,3 +72,32 @@ glm::mat4 Projection::frustum(float left, float right, float bottom, float top, 
     return result;
 }
 
+glm::mat4 Projection::viewport(float x, float y, float width, float height, float nearVal, float farVal)
+{
+    glm::mat4 result;
+
+    result[0][0] = width * 0.5f;
+    result[0][1] = 0.0f;
+    result[0][2] = 0.0f;
+    result[0][3] = 0.0f;
+
+    result[1][0] = 0.0f;
+    result[1][1] = height * 0.5f;
+    result[1][2] = 0.0f;
+    result[1][3] = 0.0f;
+
+    result[2][0] = 0.0f;
+    result[2][1] = 0.0f;
+    // z range is [0 1]
+    result[2][2] = farVal - nearVal;
+    result[2][3] = 0.0f;
+
+    result[3][0] = x + width * 0.5f;
+    result[3][1] = y + height * 0.5f;
+    // z range is [0 1]
+    result[3][2] = nearVal;
+    result[3][3] = 1.0f;
+
+    return result;
+}
+
--- TinyEngine/src/math/Projection.h
+++ TinyEngine/src/math/Projection.h
@@ -11,6 +11,7 @@ public:
 	static glm::mat4 perspective(float fovy, float aspect, float zNear, float zFar);
 	static glm::mat4 frustum(float left, float right, float bottom, float top, float nearVal, float farVal);
 
+	static glm::mat4 viewport(float x, float y, float width, float height, float nearVal, float farVal);
 };
 
 #endif /* MATH_PROJECTION_H_ */
