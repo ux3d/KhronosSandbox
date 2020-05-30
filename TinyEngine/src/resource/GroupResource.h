68b897cb9482aae391cebed6af84d4fbb5ef0ace
Norbert Nopper <nopper@ux3d.io>
2020-05-30 18:06:58 +0200

    Another abstraction.

--- TinyEngine/src/resource/GroupResource.h
+++ TinyEngine/src/resource/GroupResource.h
@@ -1,7 +1,13 @@
 #ifndef RESOURCE_GROUPRESOURCE_H_
 #define RESOURCE_GROUPRESOURCE_H_
 
+#include <cstdint>
+#include <vector>
+
 struct GroupResource {
+
+	std::vector<uint64_t> primitiveHandles;
+
 };
 
 #endif /* RESOURCE_GROUPRESOURCE_H_ */
--- TinyEngine/src/resource/InstanceResource.h
+++ TinyEngine/src/resource/InstanceResource.h
@@ -1,7 +1,16 @@
 #ifndef RESOURCE_INSTANCERESOURCE_H_
 #define RESOURCE_INSTANCERESOURCE_H_
 
+#include <cstdint>
+
+#include "../math/Math.h"
+
 struct InstanceResource {
+
+	uint64_t groupHandle;
+
+	glm::mat4 worldMatrix;
+
 };
 
 #endif /* RESOURCE_INSTANCERESOURCE_H_ */
